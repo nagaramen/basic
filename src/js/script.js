c37ee95f5116f4fd7093702a7c75f879a3e13488
Ryo Nagara <nagara@freedive.co.jp>
2020-09-09 11:18:50 +0900

    htaccess-cache-settings

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,4 @@
 $(function(){
-  if((navigator.userAgent.indexOf('iPhone') > 0 || navigator.userAgent.indexOf('Android') > 0 && navigator.userAgent.indexOf('Mobile') > 0) ||
-  (navigator.userAgent.indexOf('iPad') > 0 || navigator.userAgent.indexOf('Android') > 0)) {
-    (function(d) {
-      var config = {
-        kitId: 'fgd3szu',
-        scriptTimeout: 3000,
-        async: true
-      },
-      h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-    })(document);
-  }
-
   $('a[href^="#"]').click(function() {
     var adjust = 100;
     var href= $(this).attr("href");
