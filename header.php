b0e982b08d2ce71bd343fccc0d8e14d874838ec3
Ryo Nagara <nagara@freedive.co.jp>
2020-09-14 14:32:33 +0900

    htaccess-cache-settings	 #02

--- header.php
+++ header.php
@@ -17,6 +17,7 @@
   	<title>ヒューマンアートスクール｜Human Art School</title>
   	<meta charset="utf-8">
   	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1" />
+    <meta name="google-site-verification" content="SXJ4sf6pRNprEetBGLVSgPG_53opCuoP5OVNCdbhZh0" />
 
   	<link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.ico">
     <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/assets/img/apple-touch-icon-180x180.png">
@@ -30,10 +31,32 @@
       gtag('js', new Date());
       gtag('config', 'AW-592840185');
     </script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-177655820-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-177655820-1');
+    </script>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-W9Z9PSB');</script>
+    <!-- End Google Tag Manager -->
 		<?php wp_head(); ?>
   </head>
 
   <body class="loading">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W9Z9PSB"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
   	<header>
   		<div class="inner1200">
   			<a href="/" class="header-logo"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.svg" alt="Human Art Schoolロゴ"></a>
